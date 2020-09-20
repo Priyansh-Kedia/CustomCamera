d032d14b9648615b1cfef66ff0798931a8e9b3b8
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-09-20 20:25:49 +0530

    migrated to camerax

--- customcamera/src/main/java/com/kedia/customcamera/CustomCamera.kt
+++ customcamera/src/main/java/com/kedia/customcamera/CustomCamera.kt
@@ -1,26 +1,34 @@
 package com.kedia.customcamera
 
+import android.Manifest
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
 import android.hardware.Camera
-import android.hardware.camera2.CameraManager
 import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.SurfaceHolder
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.LinearLayout
 import androidx.annotation.LayoutRes
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.bitmap.Rotate
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 import kotlinx.android.synthetic.main.custom_camera.view.*
-import androidx.recyclerview.widget.LinearLayoutManager
+import java.io.File
+import java.lang.Math.abs
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 
-class CustomCamera : FrameLayout, SurfaceHolder.Callback {
+class CustomCamera : FrameLayout, LifecycleOwner {
 
     @LayoutRes
     private var mainLayoutId = 0
@@ -31,12 +39,72 @@ class CustomCamera : FrameLayout, SurfaceHolder.Callback {
     private lateinit var jpegCallback: Camera.PictureCallback
     private val TAG = "TAG!!!!"
 
+    private lateinit var cameraExecutor: ExecutorService
+
+    private lateinit var outputDirectory: File
+
     private lateinit var camera: Camera
 
     private val imageArrayList: MutableList<Bitmap?> = mutableListOf()
-    private val customCameraAdapter by lazy { CustomImageAdapter(context, mutableListOf()) }
-    private val linearLayoutManager by lazy { LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false) }
+//    private val customCameraAdapter by lazy { CustomImageAdapter(context, mutableListOf()) }
+//    private val linearLayoutManager by lazy { LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false) }
 
+    private var imageCapture: ImageCapture? = null
+//
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+
+        cameraProviderFuture.addListener(Runnable {
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(surfaceView.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+//            val imageAnalyzer = ImageAnalysis.Builder()
+//                .build()
+//                .also {
+//                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+//                        Log.d(TAG, "Average luminosity: $luma")
+//                    })
+//                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(context))
+    }
+
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = kotlin.math.max(width, height).toDouble() / kotlin.math.min(
+            width,
+            height
+        )
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
@@ -60,86 +128,104 @@ class CustomCamera : FrameLayout, SurfaceHolder.Callback {
 
         val view = LayoutInflater.from(context).inflate(mainLayoutId, this)
 
-        surfaceHolder = surfaceView.holder
+        startCamera()
 
-        imageRecyclerView.apply {
-            adapter = this@CustomCamera.customCameraAdapter
-            layoutManager = linearLayoutManager
-        }
-
-        surfaceHolder.addCallback(this)
-
-        jpegCallback = Camera.PictureCallback { bytes, camera ->
-
-            val decodeBitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            val now = System.currentTimeMillis()
-            val rotatedBitmap = rotateBitmap(decodeBitmap)
-
-          //  Glide.with(context).load(decodeBitmap).transform(Rotate(90)).into(capturedImage)
-            capturedImage.setImageBitmap(rotatedBitmap)
-            imageArrayList.add(rotatedBitmap)
-            customCameraAdapter.addData(rotatedBitmap)
-
-            Log.d("TAG!!!!",(imageArrayList).toString())
-            capturedImage.visibility = View.VISIBLE
-            surfaceView.visibility = View.GONE
-            captureImage.visibility = View.GONE
-            recaptureImage.visibility = View.VISIBLE
-        }
-
-        recaptureImage.setOnClickListener {
-            initialiseCamera()
-            capturedImage.visibility = View.GONE
-            surfaceView.visibility = View.VISIBLE
-            captureImage.visibility = View.VISIBLE
-            recaptureImage.visibility = View.GONE
-        }
+        outputDirectory = getOutputDirectory()
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
         captureImage.setOnClickListener {
-            captureImage()
+            takePhoto()
+            Log.d(TAG, "clicked")
         }
 
     }
 
-    private fun rotateBitmap(decodeBitmap: Bitmap?): Bitmap? {
-        val width = decodeBitmap?.width
-        val height = decodeBitmap?.height
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
 
+        Log.d(TAG, "returned")
+
+        // Create time-stamped output file to hold the image
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(FILENAME_FORMAT, Locale.getDefault()
+            ).format(System.currentTimeMillis()) + ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        Log.d(TAG, outputOptions.toString() + "    +++   " + photoFile.absoluteFile)
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+//        imageCapture.takePicture(
+//            outputOptions, ContextCompat.getMainExecutor(context), object : ImageCapture.OnImageSavedCallback {
+//                override fun onError(exc: ImageCaptureException) {
+//                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+//                }
+//
+//                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+//                    val savedUri = Uri.fromFile(photoFile)
+//                    val msg = "Photo capture succeeded: $savedUri"
+//
+//                    Log.d(TAG, msg)
+//                }
+//            })
+        imageCapture.takePicture(ContextCompat.getMainExecutor(context),object : ImageCapture.OnImageCapturedCallback(){
+            override fun onCaptureSuccess(image: ImageProxy) {
+                // Use the image, then make sure to close it.
+                Log.d(TAG, image.toString())
+                val buffer = image.planes[0].buffer
+                val bytes = ByteArray(buffer.capacity()).also { buffer.get(it) }
+                capturedImage.apply {
+                    setImageBitmap(rotateBitmap(BitmapFactory.decodeByteArray(bytes, 0, bytes.size)))
+                    visibility = View.VISIBLE
+                }
+
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                val errorType = exception.getImageCaptureError()
+
+            }
+        })
+    }
+
+    private fun rotateBitmap(decodeByteArray: Bitmap?): Bitmap? {
         val matrix = Matrix()
         matrix.postRotate(90f)
-        val rotatedImage = Bitmap.createBitmap(decodeBitmap!!, 0, 0, width!!, height!!, matrix, true)
-        decodeBitmap.recycle()
-        return rotatedImage
+        val scaledBitmap = Bitmap.createScaledBitmap(decodeByteArray!!, width, height, true)
+
+        return Bitmap.createBitmap(
+            scaledBitmap,
+            0,
+            0,
+            scaledBitmap.width,
+            scaledBitmap.height,
+            matrix,
+            true
+        )
     }
 
-    private fun captureImage() {
-        camera.takePicture(null, null, jpegCallback)
+    private fun getOutputDirectory(): File {
+        val mediaDir = context.externalMediaDirs.firstOrNull()?.let {
+            File(it, "somename").apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else context.filesDir
     }
 
-    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-        initialiseCamera()
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
 
-    private fun initialiseCamera() {
-        camera = Camera.open()
-
-        val parameters = camera.parameters
-        camera.setDisplayOrientation(90)
-        parameters.previewFrameRate = 30
-        parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE
-
-        camera.setPreviewDisplay(surfaceHolder)
-        camera.parameters = parameters
-        camera.enableShutterSound(false)
-        camera.startPreview()
-    }
-
-    override fun surfaceDestroyed(p0: SurfaceHolder) {
-
-    }
-
-    override fun surfaceCreated(p0: SurfaceHolder) {
-
+    override fun getLifecycle(): Lifecycle {
+        val lifecycleRegistry = LifecycleRegistry(this);
+        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START);
+        return lifecycleRegistry
     }
 
 }
---
+++ customcamera/src/main/java/com/kedia/customcamera/CustomCameraFragment.kt
@@ -0,0 +1,97 @@
+package com.kedia.customcamera
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide.init
+import kotlinx.android.synthetic.main.custom_camera.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CustomCameraFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CustomCameraFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_custom_camera, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        startCamera()
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(surfaceView.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment CustomCameraFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            CustomCameraFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
