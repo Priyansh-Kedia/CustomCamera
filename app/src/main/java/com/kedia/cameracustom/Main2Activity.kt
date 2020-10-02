8049e62127acf79776770fc03878b76261730af9
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-10-03 02:32:27 +0530

    changes

---
+++ app/src/main/java/com/kedia/cameracustom/Main2Activity.kt
@@ -0,0 +1,17 @@
+package com.kedia.cameracustom
+
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kedia.customcamera.CCMultiple
+
+class Main2Activity : AppCompatActivity(), CCMultiple.CustomMultiple {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    override fun onConfirmImagesClicked(imageArrayList: MutableList<Bitmap?>) {
+        TODO("Not yet implemented")
+    }
+}
